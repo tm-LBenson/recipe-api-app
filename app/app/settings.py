0068f0992ced760f9d03d29551de940d272f7e12
Lewis Benson <tm.lewis.benson@gmail.com.com>
2023-04-07 15:45:10 -0500

    put secret variable into multiple lines for Lint

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nuc@&!j3zg@r^&x71inwyf36g%o_66$t$#&s3!5y3(duhb)qah'
+SECRET_KEY = (
+    'django-insecure-nuc@&!j3zg@r^&x71inwyf36g%o_66$t$#&s3!5y3('
+    'duhb)qah'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
