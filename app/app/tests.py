5d0694aa19f0ec9d58d21f4ec84869bffbee0efa
Lewis Benson <tm.lewis.benson@gmail.com.com>
2023-04-08 17:39:29 -0500

    setup subtract function with TDD

--- app/app/tests.py
+++ app/app/tests.py
@@ -15,3 +15,8 @@ class CalcTests(SimpleTestCase):
 
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Test subtracting numbers"""
+        res = calc.subtract(10, 4)
+        self.assertEqual(res, 6)
