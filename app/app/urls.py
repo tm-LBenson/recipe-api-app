5423e06e4a3f5da5fcde4ba6258f468ede2d7aed
tm-LBenson <tm.lewisbenson@gmail.com>
2023-04-25 07:49:08 -0400

    fix formatting for linter

--- app/app/urls.py
+++ app/app/urls.py
@@ -21,8 +21,9 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),    path('api/schema/',
-                                             SpectacularAPIView.as_view(), name='api-schema'),
+    path('admin/', admin.site.urls),
+    path('api/schema/',
+         SpectacularAPIView.as_view(), name='api-schema'),
     path(
         'api/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
